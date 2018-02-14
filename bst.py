688d480384daaa33c56c3469624005e8adef0196
Cameron Neville <cameronneville1994@gmail.com>
2018-02-13 20:55:14 -0500

    fixed deletion

--- bst.py
+++ bst.py
@@ -1,7 +1,9 @@
-from random import getrandbits
+#only needed for old deletion algorithm
+#from random import getrandbits
 
 #A basic binary search tree
 #duplicate keys are not allowed; it will overwrite instead
+#all methods except successor assume they are being called by the top of the tree
 class BST:
     
     #for now I'm assuming all keys are immutable and comparable
@@ -17,17 +19,17 @@ class BST:
     def __getitem__(self, key):
         if self.key is None:
             raise KeyError('key %s does not exist' % key)
-        current = self
-        while key != current.key:
-            if key < current.key:
-                if current.left is None:
+        crnt = self
+        while key != crnt.key:
+            if key < crnt.key:
+                if crnt.left is None:
                     raise KeyError('key %s does not exist' % key)
-                current = current.left
+                crnt = crnt.left
             else:
-                if current.right is None:
+                if crnt.right is None:
                     raise KeyError('key %s does not exist' % key)
-                current = current.right
-        return current.data
+                crnt = crnt.right
+        return crnt.data
     
     #TODO replace recursion with loop
     def __setitem__(self, key, data):
@@ -49,54 +51,116 @@ class BST:
         else:
             raise KeyError('key %s cannot be compared to other keys' % key)
     
-    def __delitem__(self, key):
-        if self.key is None:
-            raise KeyError('key %s does not exist' % key)
-        current = self
-        while key != current.key:
-            if key < current.key:
-                if current.left is None:
-                    raise KeyError('key %s does not exist' % key)
-                current = current.left
-            else:
-                if current.right is None:
-                    raise KeyError('key %s does not exist' % key)
-                current = current.right
-        if current.left is None:
-            if current.right is None:
-                if current.parent is None:
-                    current.key = None
-                    current.data = None
+    #deletes given node or node corresponding to given key
+    #follows the algorithm given in CLRS
+    def __delitem__(self, entry):
+        if not isinstance(entry, BST):
+            entry = self.getnode(entry)
+        if entry.left is None:
+            #no children
+            if entry.right is None:
+                if entry.parent.left is entry:
+                    entry.parent.left = None
                 else:
-                    if current.parent.left is current:
-                        current.parent.left = None
-                    else:
-                        current.parent.right = None
+                    entry.parent.right = None
+            #right child only
             else:
-                current.key = current.right.key
-                current.data = current.right.data
-                current.left = current.right.left
-                current.right = current.right.right
-        elif current.right is None:
-            current.key = current.left.key
-            current.data = current.left.data
-            current.right = current.left.right
-            current.left = current.left.left
+                entry.key = entry.right.key
+                entry.data = entry.right.data
+                entry.left = entry.right.left
+                entry.right = entry.right.right
         else:
-            #randomly determines which child to replace itself with
-            #choosing one child consistently could lead to a lopsided list
-            if not getrandbits(1):
-                #pull left
-                current.key = current.left.key
-                current.data = current.left.key
-                current.right.mergeleft(current.left.right)
-                current.left = current.left.left
+            #left child only
+            if entry.right is None:
+                entry.key = entry.left.key
+                entry.data = entry.left.data
+                entry.right = entry.left.right
+                entry.left = entry.left.left
             else:
-                #pull right
-                current.key = current.right.key
-                current.data = current.right.data
-                current.left.mergeright(current.right.left)
-                current.right = current.right.right
+                suc = entry.successor()
+                entry.key = suc.key
+                entry.data = suc.data
+                self.newdel(suc)
+    
+    #returns node or key following the provided node or key
+    #if given a node returns a node else returns a key
+    #if entry is not defined, it is replaced with self
+    #either way, returns None if given the maximum
+    def successor(self, entry=None):
+        if entry is None:
+            entry = self
+        isnode = isinstance(entry, BST)
+        node = entry if isnode else self.getnode(entry)
+        if node.right is not None:
+            return node.right.minimum(isnode)
+        p = node.parent
+        while p is not None and p.right is None:
+            p = p.parent
+        if p.right is not None:
+            return p.minimum(isnode)
+        return None
+    
+    
+    def getnode(self, key):
+        crnt = self
+        while key != crnt.key:
+            if key < crnt.key:
+                crnt = crnt.left
+            elif key > crnt.key:
+                crnt = crnt.right
+            if crnt is None:
+                raise KeyError('key %s does not exist' % key)
+        return crnt
+    
+    #a deletion algorithm I came up with before consulting CLRS
+    # def __delitem__(self, key):
+        # if self.key is None:
+            # raise KeyError('key %s does not exist' % key)
+        # crnt = self
+        # while key != crnt.key:
+            # if key < crnt.key:
+                # if crnt.left is None:
+                    # raise KeyError('key %s does not exist' % key)
+                # crnt = crnt.left
+            # else:
+                # if crnt.right is None:
+                    # raise KeyError('key %s does not exist' % key)
+                # crnt = crnt.right
+        # if crnt.left is None:
+            # if crnt.right is None:
+                # if crnt.parent is None:
+                    # crnt.key = None
+                    # crnt.data = None
+                # else:
+                    # if crnt.parent.left is crnt:
+                        # crnt.parent.left = None
+                    # else:
+                        # crnt.parent.right = None
+            # else:
+                # crnt.key = crnt.right.key
+                # crnt.data = crnt.right.data
+                # crnt.left = crnt.right.left
+                # crnt.right = crnt.right.right
+        # elif crnt.right is None:
+            # crnt.key = crnt.left.key
+            # crnt.data = crnt.left.data
+            # crnt.right = crnt.left.right
+            # crnt.left = crnt.left.left
+        # else:
+            # #randomly determines which child to replace itself with
+            # #choosing one child consistently could lead to a lopsided list
+            # if not getrandbits(1):
+                # #pull left
+                # crnt.key = crnt.left.key
+                # crnt.data = crnt.left.key
+                # crnt.right.mergeleft(crnt.left.right)
+                # crnt.left = crnt.left.left
+            # else:
+                # #pull right
+                # crnt.key = crnt.right.key
+                # crnt.data = crnt.right.data
+                # crnt.left.mergeright(crnt.right.left)
+                # crnt.right = crnt.right.right
     
     #TODO replace recursion with loop
     def inorder(self):
@@ -108,23 +172,63 @@ class BST:
             outp += ', ' + self.right.inorder()
         return outp
     
+    #returns a generator that iterates through keys in order
+    #TODO remove recursion
+    def keys(self):
+        if self.left is not None:
+            for i in self.left.keys():
+                yield i
+        yield self.key
+        if self.right is not None:
+            for i in self.right.keys():
+                yield i
+        
+    #returns a generator that iterates through nodes in order of keys
+    #TODO remove recursion
+    def nodes(self):
+        if self.left is not None:
+            for i in self.left.nodes():
+                yield i
+        yield self
+        if self.right is not None:
+            for i in self.right.nodes():
+                yield i
+    
+    #places a subtree in the minimum available position under another tree
+    #used by my old, bad deletion algorithm
     def mergeleft(self, other):
         if other is None:
             return
-        current = self
-        while current.left is not None:
-            current = current.left
-        current.left = other
-        other.parent = current
+        crnt = self
+        while crnt.left is not None:
+            crnt = crnt.left
+        crnt.left = other
+        other.parent = crnt
     
+    #places a subtree in the maximum available position under another subtree
+    #used by my old, bad deletion algorithm
     def mergeright(self, other):
         if other is None:
             return
-        current = self
-        while current.right is not None:
-            current = current.right
-        current.right = other
-        other.parent = current
+        crnt = self
+        while crnt.right is not None:
+            crnt = crnt.right
+        crnt.right = other
+        other.parent = crnt
+    
+    #returns minimum key or node
+    def minimum(self, retnode=False):
+        crnt = self
+        while crnt.left is not None:
+            crnt = crnt.left
+        return crnt if retnode else crnt.key
+    
+    #returns maximum key or node
+    def maximum(self, retnode=False):
+        crnt = self
+        while crnt.right is not None:
+            crnt = crnt.right
+        return crnt if retnode else crnt.key
 
 #quickly makes a large tree for testing purposes
 def maketest():
